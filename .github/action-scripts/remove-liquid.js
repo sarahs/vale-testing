acaadee2684a459170af5c442fdc7ea37c80dcbd
Sarah Schneider <sarahs@github.com>
2022-04-21 18:47:05 -0400

    add script to remove Liquid and create temp files

---
+++ .github/action-scripts/remove-liquid.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import path from 'path'
+import fs from 'fs'
+import walk from 'walk-sync'
+
+// Gather markdown files
+const allFiles = walkFiles('content', '.md')
+  .concat(walkFiles('data', '.md'))
+
+// Remove Liquid and write output to .TEMP.md
+allFiles.forEach(filename => {
+  const newFilename = filename.replace('.md', '.TEMP.md')
+  const content = fs.readFileSync(filename, 'utf-8')
+  const modified = content.replaceAll(/\{%.+?%\}/g, '')
+  fs.writeFileSync(newFilename, modified)
+})
+
+function walkFiles(dir, ext, opts = {}) {
+  const dirPath = path.posix.join(process.cwd(), dir)
+  const walkSyncOpts = { includeBasePath: true, directories: false }
+
+  return walk(dirPath, walkSyncOpts)
+    .filter((file) => file.endsWith(ext) && !file.endsWith('README.md'))
+}
